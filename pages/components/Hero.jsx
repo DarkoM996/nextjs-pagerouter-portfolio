c428da8c7bfb9a5a8687ee6057baedafd1513e5c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-08 16:35:25 +0200

    Back to hero section work

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4"></div>
+    <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4">
+      <div>
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
+          Creative Developer and Designer
+        </h1>
+      </div>
+    </div>
   );
 };
 
