a7633d31cc70031bc5247dd4a2090a4342c2cfff
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-08 16:14:41 +0200

    Extending Tailwind with custom colors per design

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1920px",
+      },
+      colors: {
+        primary_backgroundLighterColor: {
+          100: "#F1F1F1",
+          200: "#EDEDED",
+          300: "#E6E6E6",
+          400: "#E0E0E0",
+          500: "#D9D9D9",
+        },
+        primary_backgroundDarkerColor: {
+          600: "#262626",
+          700: "#1F1F1F",
+          800: "#191919",
+          850: "#121212",
+          900: "#0D0C0C",
+        },
+      },
+    },
   },
   plugins: [],
 };
