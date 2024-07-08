34a9f2cf40faeacc80a2a9620048b15ea846ff60
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-08 16:07:27 +0200

    Initial component added

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <h4 className="text-3xl font-bold">Hello From Navbar</h4>
+    </div>
+  );
 };
 
 export default Navbar;
