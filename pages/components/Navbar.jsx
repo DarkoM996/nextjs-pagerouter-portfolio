9699be50e1a137000736f0ec71588a042e3d435e
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-08 16:33:48 +0200

    renaming it to nav here instead of div

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="max-w-screen-3xl mx-auto flex justify-between items-center w-full h-20 px-6 py-4">
+    <nav className="max-w-screen-3xl mx-auto flex justify-between items-center w-full h-20 px-6 py-4">
       <Link href="/" className="text-xl font-bold text-wrap hover:underline">
         Darko Mihajlovski
       </Link>
       <Link href="/about" className="text-xl font-semibold hover:underline">
         About
       </Link>
-    </div>
+    </nav>
   );
 };
 
