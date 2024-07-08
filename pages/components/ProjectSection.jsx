ce2d33f02def7a172536fcebf5349e7de91655f7
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-08 16:49:35 +0200

    h-screen works, h-full here doesn't work

--- pages/components/ProjectSection.jsx
+++ pages/components/ProjectSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProjectSection = () => {
   return (
-    <div className="max-w-screen-3xl mx-auto w-full h-full relative px-6 py-4">
+    <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4">
       <h2 className="text-4xl md:text-5xl lg:text-6xl font-semibold">
         Project Sections
       </h2>
