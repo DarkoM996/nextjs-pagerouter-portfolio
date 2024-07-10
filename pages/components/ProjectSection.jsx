5e71ca670238abf80ac8fc604ef865606eac4719
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-10 19:43:16 +0200

    another design test

--- pages/components/ProjectSection.jsx
+++ pages/components/ProjectSection.jsx
@@ -10,7 +10,7 @@ import Link from "next/link";
 const ProjectSection = () => {
   return (
     <div className=" max-w-screen-2xl mx-auto h-[1600px] relative px-6 py-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 lg:gap-24">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 lg:gap-24 lg:px-32">
         {/* Project #1 */}
         <div className="flex flex-col justify-center items-center gap-4 md:mb-40">
           <Link
