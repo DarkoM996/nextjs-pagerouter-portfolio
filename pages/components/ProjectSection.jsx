f472be1d70eee8965cdc60462983c30aac5863bd
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-10 19:37:03 +0200

    Design test for width

--- pages/components/ProjectSection.jsx
+++ pages/components/ProjectSection.jsx
@@ -9,8 +9,8 @@ import Link from "next/link";
 
 const ProjectSection = () => {
   return (
-    <div className=" max-w-screen-2xl mx-auto h-[1600px] relative px-6 py-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 lg:gap-48">
+    <div className=" max-w-screen-xl mx-auto h-[1600px] relative px-6 py-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 lg:gap-24">
         {/* Project #1 */}
         <div className="flex flex-col justify-center items-center gap-4 md:mb-40">
           <Link
