bba6fef8a212b0f224ec5369f0416202f4786c78
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 18:09:01 +0200

    Getting closer to the design

--- pages/components/AboutHero.jsx
+++ pages/components/AboutHero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AboutHero = () => {
   return (
     <div className="max-w-screen-xl mx-auto w-full h-full relative px-6 py-4 lg:py-24">
-      <div className="flex flex-col justify-center items-start w-full h-full">
+      <div className="max-w-screen-lg mx-auto flex flex-col justify-center items-start w-full h-full lg:px-32">
         <h1 className="text-4xl md:text-5xl font-semibold">
           Product Designer specializing in user-centric innovation and product
           evolution
