b832a2c4f3c737dba19ac9c9d61347a93880f221
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 16:52:34 +0200

    Notes + off to have lunch

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,10 @@ import React from "react";
 const about = () => {
   return (
     <div className="max-w-screen-xl mx-auto w-full min-h-screen relative px-6 py-4">
-      about
+      {/* Initial Above the fold part */}
+      <div>
+        <h1>This needs to have the same construction as in index.js</h1>
+      </div>
     </div>
   );
 };
