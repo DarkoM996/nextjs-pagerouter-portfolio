594f3d512fbd5157ace9e24fbc41e39b1fc15b4f
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 17:30:55 +0200

    Adjusting text size in paragraphs

--- pages/components/AboutText.jsx
+++ pages/components/AboutText.jsx
@@ -3,27 +3,27 @@ import React from "react";
 const AboutText = () => {
   return (
     <div className="max-w-screen-xl mx-auto w-full min-h-screen relative px-6 py-4">
-      {/* First Paragraph */}
-      <div>
-        <p className="text-2xl">
+      {/* Paragraphs that give explanations */}
+      <div className="flex flex-col justify-center items-center gap-8">
+        <p className="text-xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
           voluptate, assumenda officiis nam deleniti natus corporis repellat
           accusamus tempora voluptates obcaecati sed, iure voluptatem doloremque
           animi omnis, saepe vero doloribus.
         </p>
-      </div>
-      {/* First Paragraph */}
-      <div>
-        <p>
+        <p className="text-xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
           voluptate, assumenda officiis nam deleniti natus corporis repellat
           accusamus tempora voluptates obcaecati sed, iure voluptatem doloremque
           animi omnis, saepe vero doloribus.
         </p>
-      </div>
-      {/* First Paragraph */}
-      <div>
-        <p>
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+          voluptate, assumenda officiis nam deleniti natus corporis repellat
+          accusamus tempora voluptates obcaecati sed, iure voluptatem doloremque
+          animi omnis, saepe vero doloribus.
+        </p>
+        <p className="text-xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
           voluptate, assumenda officiis nam deleniti natus corporis repellat
           accusamus tempora voluptates obcaecati sed, iure voluptatem doloremque
