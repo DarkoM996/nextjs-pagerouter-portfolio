221427a88ebb3aa8c4b3f75ae364cfb800162fd6
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 16:35:54 +0200

    New default font test

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
 import "@/styles/globals.css";
-import { Work_Sans } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 
-const worksans = Work_Sans({
+const opensans = Open_Sans({
   subsets: ["latin"],
   display: "swap",
   variable: "--font--bodoni--moda",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["300", "400", "500", "600", "700", "800"],
 });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={worksans.className}>
+    <main className={opensans.className}>
       <Component {...pageProps} />
     </main>
   );
