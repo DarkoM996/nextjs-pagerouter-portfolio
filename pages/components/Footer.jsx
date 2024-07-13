4e29017eb6c3ce0fae0c95e3b869c9cdfa12d8a0
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 15:46:33 +0200

    Changing text color on text

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { FaBehance, FaDribbble, FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="text-black">
-      <div>
-        <p>&copy Darko Mihajlovski</p>
+    <div className="max-w-screen-xl mx-auto w-full h-full relative px-6 py-4">
+      <div className="flex flex-col md:flex-row justify-between items-center  gap-6">
+        <p className="text-primary_customGray-700">&copy; Darko Mihajlovski</p>
         {/* Icons with links pointing out to socials */}
-        <div></div>
+        <div className="flex items-center gap-8 text-2xl">
+          <a href="https://github.com/DarkoM996">
+            <FaGithub />
+          </a>
+          <a href="https://www.behance.net/darkomihajlo">
+            <FaBehance />
+          </a>
+          <a href="https://dribbble.com/DarkoM_0-10">
+            <FaDribbble />
+          </a>
+          <a href="https://www.linkedin.com/in/darko-mihajlovski-1bb4701b7">
+            <FaLinkedinIn />
+          </a>
+        </div>
       </div>
     </div>
   );
