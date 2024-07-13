a070347097aac8c3686bb4871e31ebe570677280
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 17:17:02 +0200

    Hero section in the about page done

--- pages/components/AboutHero.jsx
+++ pages/components/AboutHero.jsx
@@ -3,10 +3,11 @@ import React from "react";
 const AboutHero = () => {
   return (
     <div className="max-w-screen-xl mx-auto w-full min-h-screen relative px-6 py-4">
-      <h1>
-        Senior Product Designer specializing in user-centric innovation and
-        product evolution.
-      </h1>
+      <div className="flex flex-col justify-center items-start w-full h-full">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold mt-32">
+          Web Development. Web Design. Product Design & More
+        </h1>
+      </div>
     </div>
   );
 };
