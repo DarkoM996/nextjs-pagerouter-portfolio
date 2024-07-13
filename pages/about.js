8f313f09598ab2cad15de15bdfce5921eaf602ae
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 17:12:00 +0200

    New structure

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,11 @@
 import React from "react";
+import AboutHero from "./components/AboutHero";
 
 const about = () => {
   return (
-    <div className="max-w-screen-xl mx-auto w-full min-h-screen relative px-6 py-4">
-      {/* Initial Above the fold part */}
-      <div>
-        <h1>This needs to have the same construction as in index.js</h1>
-      </div>
-    </div>
+    <>
+      <AboutHero />
+    </>
   );
 };
 
