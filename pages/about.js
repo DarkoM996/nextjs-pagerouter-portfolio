862142ad0e6d5fa4c96f75801047ba627ba464f4
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 16:47:22 +0200

    About Page work begins

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <div className="max-w-screen-xl mx-auto w-full min-h-screen relative px-6 py-4">
+      about
+    </div>
+  );
 };
 
 export default about;
