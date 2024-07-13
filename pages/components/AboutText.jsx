c4514088c2c180f076ef28733bce13949acf8b31
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 17:18:55 +0200

    latest

---
+++ pages/components/AboutText.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutText = () => {
+  return (
+    <div className="max-w-screen-xl mx-auto w-full min-h-screen relative px-6 py-4">
+      AboutText
+    </div>
+  );
+};
+
+export default AboutText;
