fa7e0cb577ae692ded43f04f56218d64a302227d
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 17:26:53 +0200

    Working title

--- pages/components/AboutHero.jsx
+++ pages/components/AboutHero.jsx
@@ -4,8 +4,9 @@ const AboutHero = () => {
   return (
     <div className="max-w-screen-xl mx-auto w-full h-full relative px-6 py-4 lg:py-24">
       <div className="flex flex-col justify-center items-start w-full h-full">
-        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
-          Web Development. Web Design. Product Design & More
+        <h1 className="text-4xl md:text-5xl font-semibold">
+          Product Designer specializing in user-centric innovation and product
+          evolution
         </h1>
       </div>
     </div>
