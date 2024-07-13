a2487d0e5c5d08cf50aec8b36bf354dc3eb69dd4
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 15:37:05 +0200

    Installing react icons package

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="text-black">Footer</div>;
+  return (
+    <div className="text-black">
+      <div>
+        <p>&copy Darko Mihajlovski</p>
+        {/* Icons with links pointing out to socials */}
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
