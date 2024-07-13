7a6ecf535952ee37a5316751d0019e8adf08d377
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 18:04:35 +0200

    Working section

--- pages/components/AboutText.jsx
+++ pages/components/AboutText.jsx
@@ -5,23 +5,23 @@ const AboutText = () => {
     <div className="max-w-screen-xl mx-auto w-full min-h-screen relative px-6 py-4">
       {/* Paragraphs that give explanations */}
       <div className="max-w-screen-md mx-auto w-full h-full flex flex-col justify-center items-center gap-8">
-        <p className="text-xl">
+        <p className="text-xl text-black font-light ">
           I am Darko Mihajlovski, a creative developer and designer based out of
           Kumanovo, North Macedonia. I help companies and brands with specific
           solutions while focusing on taking my work to the next level with
           quality as the main goal.
         </p>
-        <p className="text-xl">
+        <p className="text-xl text-black font-light ">
           I build websites and web apps that focus on clean and minimalistic
           design. My focus is on clean and minimalistic design with focus on
           well placed animations, transitions as well as interaction.
         </p>
-        <p className="text-xl">
+        <p className="text-xl text-black font-light ">
           From marketing websites that inspire, to e-commerce that convinces and
           web apps that are truly beneficial. All while focusing on clean and
           minimalistic design to deliver use friendly designs for the web.
         </p>
-        <p className="text-xl">
+        <p className="text-xl text-black font-light ">
           With my knowledge as a digital designer, I make sure that a visual
           identity can be used throughout all digital touchpoints in an
           accessible and user-friendly way. I build scalable websites from
