d3c8af66fa7e053659309a91e65f05ff7ef45edc
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 22:35:13 +0200

    Initial case study structure done

--- pages/projects/furnico.js
+++ pages/projects/furnico.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import picture1 from "../../public/images/furniturewebsite-featured-pic-1.2.jpg";
+import picture2 from "../../public/images/furniturewebsite-featured-pic-1.3.jpg";
+import picture3 from "../../public/images/furniturewebsite-featured-pic-1.4.jpg";
+import picture4 from "../../public/images/furniturewebsite-featured-pic-1.5.jpg";
+import picture5 from "../../public/images/furniturewebsite-featured-pic-1.6.jpg";
+import picture6 from "../../public/images/furniturewebsite-featured-pic-1.7.jpg";
+import picture7 from "../../public/images/furniturewebsite-featured-pic-2.5.jpg";
+
+import Image from "next/image";
+import Link from "next/link";
+import Footer from "../components/Footer";
 
 const furnico = () => {
   return (
     <>
       <Navbar />
-      <div className="max-w-screen-md mx-auto w-full min-h-screen relative px-6 py-4">
+      <div className="max-w-screen-xl mx-auto w-full min-h-screen relative px-6 py-4">
         {/* Hero Section */}
-        <div className="flex flex-col justify-center items-start gap-6 md:gap-10 lg:gap-16">
+        <div className="max-w-screen-md mx-auto w-full h-full flex flex-col justify-center items-start gap-6 md:gap-10 lg:gap-16">
           <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
             Furnico Web App
           </h1>
@@ -19,7 +30,7 @@ const furnico = () => {
           </h3>
         </div>
         {/* Paragraphs explaining stuff */}
-        <div className="flex flex-col justify-center items-start gap-6 py-16">
+        <div className="max-w-screen-md mx-auto w-full h-full flex flex-col justify-center items-start gap-6 py-16">
           <p className="text-xl leading-normal text-primary_customGray-900 ">
             Days started as a side project back in 2015 when I entered and won a
             design contest. This event sparked the beginning of a new business
@@ -48,6 +59,79 @@ const furnico = () => {
             and, above all, learning, to ensure our users' satisfaction.
           </p>
         </div>
+        {/* Image #1 */}
+        <div className="relative w-full h-screen py-12 ">
+          <Image
+            priority={true}
+            src={picture1}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
+        {/* Image #2 */}
+        <div className="relative w-full h-screen py-4 ">
+          <Image
+            priority={true}
+            src={picture2}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
+        {/* Image #3 */}
+        <div className="relative w-full h-screen py-4 ">
+          <Image
+            priority={true}
+            src={picture3}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
+        {/* Image #4 */}
+        <div className="relative w-full h-screen py-4 ">
+          <Image
+            priority={true}
+            src={picture4}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
+        {/* Image #5 */}
+        <div className="relative w-full h-screen py-4 ">
+          <Image
+            priority={true}
+            src={picture5}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
+        {/* Image #6 */}
+        <div className="relative w-full h-screen py-4 ">
+          <Image
+            priority={true}
+            src={picture6}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
+        {/* Image #7 */}
+        <div className="relative w-full h-screen py-4 ">
+          <Image
+            priority={true}
+            src={picture7}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
+        {/* Back To All Projects */}
+        <div className="flex flex-col justify-center items-center w-full h-[600px]">
+          <Link
+            href="/"
+            className="text-3xl md:text-4xl lg:text-5xl font-semibold hover:underline"
+          >
+            Back To Projects
+          </Link>
+        </div>
+        <Footer />
       </div>
     </>
   );
