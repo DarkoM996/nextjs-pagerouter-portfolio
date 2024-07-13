91a38338ad1b7e4a8b4c52092b813c2d79ea8d57
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-13 15:14:02 +0200

    latest

--- pages/components/AboutSection.jsx
+++ pages/components/AboutSection.jsx
@@ -6,20 +6,24 @@ const AboutSection = () => {
     <div className="max-w-screen-xl mx-auto w-full min-h-screen relative px-6 py-4">
       <div className="flex flex-col justify-between items-center w-full h-full gap-48">
         <div className="flex flex-col justify-center items-center gap-4">
+          {/* Small paragraph section */}
           <p className="text-xl w-full md:w-1/2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa rerum
             totam suscipit pariatur velit, repudiandae ipsam laboriosam facere
             adipisci quae dolore facilis quas ratione ad, optio autem nostrum
             amet ex!
           </p>
+          {/* Link towards the other page */}
           <Link href="/about" className="text-2xl font-semibold">
             More about me
           </Link>
         </div>
+        {/* Get in touch section */}
         <div className="flex flex-col justify-center items-center gap-2 w-full h-full">
           <p className="text-base text-primary_customGray-800 w-full h-fit text-center">
             Get in touch
           </p>
+          {/* Add a link pointing towards the email */}
           <div className="flex flex-col justify-center items-center w-full h-full">
             <h2 className="text-4xl text-pretty">My Email</h2>
           </div>
