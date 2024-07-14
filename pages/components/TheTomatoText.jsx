cf6a5ea334f2ebe0e874642f67e6c277c3b6a961
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-14 12:48:52 +0200

    Initial text added

---
+++ pages/components/TheTomatoText.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const TheTomatoText = () => {
+  return (
+    <div className="max-w-screen-md mx-auto w-full h-full flex flex-col justify-center items-start gap-6 relative px-6 py-4 ">
+      <p className="text-xl leading-normal text-primary_customGray-900 ">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet obcaecati
+        cum laboriosam quos laudantium, veniam impedit cupiditate sequi numquam
+        atque ab natus similique vitae iure illo, ut aliquam, hic autem.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet obcaecati
+        cum laboriosam quos laudantium, veniam impedit cupiditate sequi numquam
+        atque ab natus similique vitae iure illo, ut aliquam, hic autem.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet obcaecati
+        cum laboriosam quos laudantium, veniam impedit cupiditate sequi numquam
+        atque ab natus similique vitae iure illo, ut aliquam, hic autem.
+      </p>
+    </div>
+  );
+};
+
+export default TheTomatoText;
--- pages/projects/thetomato.js
+++ pages/projects/thetomato.js
@@ -1,10 +1,12 @@
 import React from "react";
+import TheTomatoText from "../components/TheTomatoText";
 import TheTomatoHero from "./../components/TheTomatoHero";
 
 const thetomato = () => {
   return (
     <>
       <TheTomatoHero />
+      <TheTomatoText />
     </>
   );
 };
