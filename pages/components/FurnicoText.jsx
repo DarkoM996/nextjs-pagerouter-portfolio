107f8b06ac4a37172ae988af4d7f7621e2226b95
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-14 18:35:46 +0200

    Second case study done

--- pages/components/FurnicoText.jsx
+++ pages/components/FurnicoText.jsx
@@ -4,31 +4,26 @@ const FurnicoText = () => {
   return (
     <div className="max-w-screen-md mx-auto w-full h-full flex flex-col justify-center items-start gap-6 px-6 py-8">
       <p className="text-xl leading-normal text-primary_customGray-900 ">
-        Days started as a side project back in 2015 when I entered and won a
-        design contest. This event sparked the beginning of a new business
-        venture and a lasting friendship with Matt Davenport, an iOS developer
-        from Manchester, UK. Over the years, we've teamed up on various iOS
-        projects, but Days has always remained our primary focus and passion.
-        We're committed to constant improvement, iteration, and, above all,
-        learning, to ensure our users' satisfaction.
+        Furnico is an ecommerce full stack web app that I designed in Figma and
+        for which I made mockups in Photoshop as well. I decided to turn this
+        project into a full stack ecommerce web app that I'm building in Mongo
+        DB, Express, React and Node.Js aka the famous MERN stack. The process is
+        well underway but today, the case study will talk about the design and
+        the decisions relating to this process.
       </p>
       <p className="text-xl leading-normal text-primary_customGray-900  ">
-        Days started as a side project back in 2015 when I entered and won a
-        design contest. This event sparked the beginning of a new business
-        venture and a lasting friendship with Matt Davenport, an iOS developer
-        from Manchester, UK. Over the years, we've teamed up on various iOS
-        projects, but Days has always remained our primary focus and passion.
-        We're committed to constant improvement, iteration, and, above all,
-        learning, to ensure our users' satisfaction.
+        Furnico started as a new and fresh take on designing an ecommerce web
+        app for a fictional brand. The goal was to create a well designed and
+        visiually stunning website that is easy to use. To achieve this, I opted
+        for a brown color palette with a clean and modern layout filled with
+        images with rounded corners, buttons etc so the user can have a warm and
+        cosy feeling while navigating through the web app.
       </p>
       <p className="text-xl leading-normal text-primary_customGray-900  ">
-        Days started as a side project back in 2015 when I entered and won a
-        design contest. This event sparked the beginning of a new business
-        venture and a lasting friendship with Matt Davenport, an iOS developer
-        from Manchester, UK. Over the years, we've teamed up on various iOS
-        projects, but Days has always remained our primary focus and passion.
-        We're committed to constant improvement, iteration, and, above all,
-        learning, to ensure our users' satisfaction.
+        For now, I only have the designs to show. I made the mockups in
+        Photoshop to showcase this design. The process of bringing this app to
+        the web is underway with the usage of the MERN stack. Until that is
+        done, checkout the pictures of the design below.
       </p>
     </div>
   );
