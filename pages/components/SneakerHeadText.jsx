4f663839749be960715602ae3fdf1cae9fb60bac
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-14 11:57:48 +0200

    Section added

---
+++ pages/components/SneakerHeadText.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SneakerHeadText = () => {
+  return (
+    <div className="max-w-screen-md mx-auto w-full h-full relative px-6 py-4">
+      SneakerHeadText
+    </div>
+  );
+};
+
+export default SneakerHeadText;
--- pages/projects/sneakerhead.js
+++ pages/projects/sneakerhead.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SneakerHeadHero from "../components/SneakerHeadHero";
+import SneakerHeadText from "../components/SneakerHeadText";
 
 const sneakerhead = () => {
   return (
     <>
       <SneakerHeadHero />
+      <SneakerHeadText />
     </>
   );
 };
