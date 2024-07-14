1f85682541877e8a729b9160278c1e9015487c7b
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-14 21:01:06 +0200

    latest

--- pages/components/CoffeeBarText.jsx
+++ pages/components/CoffeeBarText.jsx
@@ -4,29 +4,60 @@ const CoffeeBarText = () => {
   return (
     <div className="max-w-screen-md mx-auto w-full h-full flex flex-col justify-center items-start gap-6 relative px-6 py-4">
       <p className="text-xl leading-normal text-primary_customGray-900 ">
-        @hotel, formerly known as TripScout, is a pioneering travel booking
-        platform that prioritizes user experience above all else. With a massive
-        social media presence and an active user base, @hotel offers unmatched
-        value with better prices, curated community insights, and superior
-        customer service. Recognized as the Travel Industry's Innovation of the
-        Year, @hotel has revolutionized the way travelers explore destinations
-        and plan their trips.
+        The idea behind this project came after the regular return from the
+        Covid Pandemic where the coffee bars experienced a strong hit in terms
+        of making money and forcing them to be shut down so they could barely
+        survive. I decided to put an idea to the test: How would such a business
+        fare if they had online presence where people can order a cup of online
+        and drink on the go, at the shop or even home? And thus I began to work
+        on this case study, calling it: CoffeeBar.
       </p>
       <p className="text-xl leading-normal text-primary_customGray-900  ">
-        After the rebranding to @hotel, the platform underwent a transformative
-        shift into a pioneering booking platform, offering exclusive rates and
-        deals to its members. As a product designer, my role was instrumental in
-        enhancing user experiences through comprehensive research, user testing,
-        and crafting intuitive user flows. I collaborated closely with
-        developers to translate design concepts into functional products.
+        Initially I decided to hit Google as a starting point just so I could
+        get some form of secondary research that'd would get me some starting
+        information on the local shops. The data that I gathered showed a
+        decrease of 90% profit and in some cases, even higher. On the other
+        hand, the e-commerce association in Macedonia showed a major improvement
+        of people whose shopping habbits online transformed dramatically. Going
+        from people who were scepticals to the ones that embraced. How much was
+        the question?
       </p>
       <p className="text-xl leading-normal text-primary_customGray-900  ">
-        In addition to my design responsibilities, I also took on cross-team
-        initiatives, drove conversations, crafted prototypes to aid
-        decision-making, and successfully secured buy-in from stakeholders. I
-        led entire design initiatives from conception to production, involving
-        developers early in the process to gather their feedback, technical
-        constraints, and resource estimates.
+        Initial research was needed to better understand the users' needs
+        related to coffee drinking and to better understand the coffee drinking
+        culture within my city.I made a mini questionare of several questions
+        and sent it out to several close people who were all between the target
+        ages of 22-34 ages with the income and professions that would respond
+        positively to this idea.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        A series of in person interviews were then conducted on 6 participants
+        to identify their pain points, frustrations, needs and desires within
+        the existing coffee culture to determine how the CoffeeBar could improve
+        this experience. Following, I went in the next phase which was to
+        analyze the gathered data and to start out the design process based on
+        that.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        Finally after so long, I finally got to the design part. The focus was
+        to design a clear web app with minimal and clean design focusing on
+        simplicity combined with visual beauty. I showed some initial designs to
+        the target audience which responded postively overall.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        Ultimately the coffee drinking culture in Macedonia as well as the
+        Balkans is unique. It's social importance is even greater than I
+        realized as majority of the coffee drinkers enjoy a cup of coffee in a
+        coffee bar for more reasons than just the coffee such as companionship,
+        quality time with a loved one etc.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        There were some signs of a potential investment, but that'd be at least
+        a decade away if not more. One option would be to offer a way to book a
+        table at a customer's specific wanted time etc. Overall duo to unique
+        coffee drinking culture, expensive distruction and just not enough
+        interest, the Starbucks like business method is not profitable at the
+        moment. Here are the designs that I came up with.
       </p>
     </div>
   );
