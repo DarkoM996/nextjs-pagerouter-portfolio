7566e113209d7bf880c3af63975c7b3f83e7226b
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-14 11:17:17 +0200

    padding for large screens

--- pages/components/FurnicoHero.jsx
+++ pages/components/FurnicoHero.jsx
@@ -4,7 +4,7 @@ const FurnicoHero = () => {
   return (
     // In this case h-full is used so the space between the sections is what I needed per design requirements
     <div className="max-w-screen-md mx-auto w-full h-full relative px-6 py-8">
-      <div className="flex flex-col justify-center items-start gap-6">
+      <div className="flex flex-col justify-center items-start gap-6 lg:gap-16 lg:py-16">
         <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
           Furnico Web App
         </h1>
