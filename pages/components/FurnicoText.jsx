c3d8fe14a48c86add354101968336fc3a6b19642
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-14 11:11:37 +0200

    Text reworked and added

--- pages/components/FurnicoText.jsx
+++ pages/components/FurnicoText.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const FurnicoText = () => {
-  return <div>FurnicoText</div>;
+  return (
+    <div className="max-w-screen-md mx-auto w-full h-full flex flex-col justify-center items-start gap-6 py-16">
+      <p className="text-xl leading-normal text-primary_customGray-900 ">
+        Days started as a side project back in 2015 when I entered and won a
+        design contest. This event sparked the beginning of a new business
+        venture and a lasting friendship with Matt Davenport, an iOS developer
+        from Manchester, UK. Over the years, we've teamed up on various iOS
+        projects, but Days has always remained our primary focus and passion.
+        We're committed to constant improvement, iteration, and, above all,
+        learning, to ensure our users' satisfaction.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        Days started as a side project back in 2015 when I entered and won a
+        design contest. This event sparked the beginning of a new business
+        venture and a lasting friendship with Matt Davenport, an iOS developer
+        from Manchester, UK. Over the years, we've teamed up on various iOS
+        projects, but Days has always remained our primary focus and passion.
+        We're committed to constant improvement, iteration, and, above all,
+        learning, to ensure our users' satisfaction.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        Days started as a side project back in 2015 when I entered and won a
+        design contest. This event sparked the beginning of a new business
+        venture and a lasting friendship with Matt Davenport, an iOS developer
+        from Manchester, UK. Over the years, we've teamed up on various iOS
+        projects, but Days has always remained our primary focus and passion.
+        We're committed to constant improvement, iteration, and, above all,
+        learning, to ensure our users' satisfaction.
+      </p>
+    </div>
+  );
 };
 
 export default FurnicoText;
--- pages/projects/furnico.js
+++ pages/projects/furnico.js
@@ -12,118 +12,88 @@ import Image from "next/image";
 import Link from "next/link";
 import Footer from "../components/Footer";
 import FurnicoHero from "../components/FurnicoHero";
+import FurnicoText from "../components/FurnicoText";
 
 const furnico = () => {
   return (
     <>
-      <div className="max-w-screen-xl mx-auto w-full min-h-screen relative px-6 py-4">
-        <FurnicoHero />
-        {/* Paragraphs explaining stuff */}
-        <div className="max-w-screen-md mx-auto w-full h-full flex flex-col justify-center items-start gap-6 py-16">
-          <p className="text-xl leading-normal text-primary_customGray-900 ">
-            Days started as a side project back in 2015 when I entered and won a
-            design contest. This event sparked the beginning of a new business
-            venture and a lasting friendship with Matt Davenport, an iOS
-            developer from Manchester, UK. Over the years, we've teamed up on
-            various iOS projects, but Days has always remained our primary focus
-            and passion. We're committed to constant improvement, iteration,
-            and, above all, learning, to ensure our users' satisfaction.
-          </p>
-          <p className="text-xl leading-normal text-primary_customGray-900  ">
-            Days started as a side project back in 2015 when I entered and won a
-            design contest. This event sparked the beginning of a new business
-            venture and a lasting friendship with Matt Davenport, an iOS
-            developer from Manchester, UK. Over the years, we've teamed up on
-            various iOS projects, but Days has always remained our primary focus
-            and passion. We're committed to constant improvement, iteration,
-            and, above all, learning, to ensure our users' satisfaction.
-          </p>
-          <p className="text-xl leading-normal text-primary_customGray-900  ">
-            Days started as a side project back in 2015 when I entered and won a
-            design contest. This event sparked the beginning of a new business
-            venture and a lasting friendship with Matt Davenport, an iOS
-            developer from Manchester, UK. Over the years, we've teamed up on
-            various iOS projects, but Days has always remained our primary focus
-            and passion. We're committed to constant improvement, iteration,
-            and, above all, learning, to ensure our users' satisfaction.
-          </p>
-        </div>
-        {/* Image #1 */}
-        <div className="relative w-full h-screen py-12 ">
-          <Image
-            priority={true}
-            src={picture1}
-            alt="/"
-            className="object-cover w-full h-full rounded-3xl"
-          />
-        </div>
-        {/* Image #2 */}
-        <div className="relative w-full h-screen py-4 ">
-          <Image
-            priority={true}
-            src={picture2}
-            alt="/"
-            className="object-cover w-full h-full rounded-3xl"
-          />
-        </div>
-        {/* Image #3 */}
-        <div className="relative w-full h-screen py-4 ">
-          <Image
-            priority={true}
-            src={picture3}
-            alt="/"
-            className="object-cover w-full h-full rounded-3xl"
-          />
-        </div>
-        {/* Image #4 */}
-        <div className="grid grid-cols-2 gap-6">
-          <div className="relative w-full h-screen py-4 ">
-            <Image
-              priority={true}
-              src={picture4}
-              alt="/"
-              className="object-cover w-full h-full rounded-3xl"
-            />
-          </div>
-          {/* Image #5 */}
-          <div className="relative w-full h-screen py-4 ">
-            <Image
-              priority={true}
-              src={picture5}
-              alt="/"
-              className="object-cover w-full h-full rounded-3xl"
-            />
-          </div>
-        </div>
-        {/* Image #6 */}
-        <div className="relative w-full h-screen py-4 ">
-          <Image
-            priority={true}
-            src={picture6}
-            alt="/"
-            className="object-cover w-full h-full rounded-3xl"
-          />
-        </div>
-        {/* Image #7 */}
-        <div className="relative w-full h-screen py-4 ">
-          <Image
-            priority={true}
-            src={picture7}
-            alt="/"
-            className="object-cover w-full h-full rounded-3xl"
-          />
-        </div>
-        {/* Back To All Projects */}
-        <div className="flex flex-col justify-center items-center w-full h-[600px]">
-          <Link
-            href="/"
-            className="text-3xl md:text-4xl  font-semibold hover:underline"
-          >
-            Back To Projects
-          </Link>
-        </div>
-        <Footer />
+      <FurnicoHero />
+      <FurnicoText />
+      {/* Image #1 */}
+      <div className="relative w-full h-screen py-12 ">
+        <Image
+          priority={true}
+          src={picture1}
+          alt="/"
+          className="object-cover w-full h-full rounded-3xl"
+        />
       </div>
+      {/* Image #2 */}
+      <div className="relative w-full h-screen py-4 ">
+        <Image
+          priority={true}
+          src={picture2}
+          alt="/"
+          className="object-cover w-full h-full rounded-3xl"
+        />
+      </div>
+      {/* Image #3 */}
+      <div className="relative w-full h-screen py-4 ">
+        <Image
+          priority={true}
+          src={picture3}
+          alt="/"
+          className="object-cover w-full h-full rounded-3xl"
+        />
+      </div>
+      {/* Image #4 */}
+      <div className="grid grid-cols-2 gap-6">
+        <div className="relative w-full h-screen py-4 ">
+          <Image
+            priority={true}
+            src={picture4}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
+        {/* Image #5 */}
+        <div className="relative w-full h-screen py-4 ">
+          <Image
+            priority={true}
+            src={picture5}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
+      </div>
+      {/* Image #6 */}
+      <div className="relative w-full h-screen py-4 ">
+        <Image
+          priority={true}
+          src={picture6}
+          alt="/"
+          className="object-cover w-full h-full rounded-3xl"
+        />
+      </div>
+      {/* Image #7 */}
+      <div className="relative w-full h-screen py-4 ">
+        <Image
+          priority={true}
+          src={picture7}
+          alt="/"
+          className="object-cover w-full h-full rounded-3xl"
+        />
+      </div>
+      {/* Back To All Projects */}
+      <div className="flex flex-col justify-center items-center w-full h-[600px]">
+        <Link
+          href="/"
+          className="text-3xl md:text-4xl  font-semibold hover:underline"
+        >
+          Back To Projects
+        </Link>
+      </div>
+      <Footer />
     </>
   );
 };
