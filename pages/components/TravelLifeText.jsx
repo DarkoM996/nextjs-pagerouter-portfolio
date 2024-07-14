eaaa12c55a8783473b97f5306086716b049af1b1
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-14 18:23:09 +0200

    One case study final design and build done

--- pages/components/TravelLifeText.jsx
+++ pages/components/TravelLifeText.jsx
@@ -5,30 +5,51 @@ const TravelLifeText = () => {
     // H-full aka height:100% gives the desired design effect here
     <div className="max-w-screen-md mx-auto w-full h-full relative flex flex-col justify-center items-start gap-6 px-6 py-4">
       <p className="text-xl leading-normal text-primary_customGray-900 ">
-        The New York Times, USA Today, and Travel & Leisure have said Tripscout
-        is THE essential app for the modern traveler. Hundreds of travel
-        bloggers and publications have featured Tripscout as the essential
-        resource for the modern traveler and have given the app some very high
-        endorsements.
+        TravelLife started as a simple design hero exploration exercise. Once I
+        finished the initial hero section design, this inspired me to continue
+        designing and create two full design case studies following this layout.
+        In this case study, I will be speaking about the first design
+        exploration and that is TravelLife.
       </p>
       <p className="text-xl leading-normal text-primary_customGray-900  ">
-        Before the rebranding of Tripscout to @hotel, my role involved enhancing
-        the user experience of the Tripscout iOS app, designed to empower users
-        to explore destinations like local insiders. With a focus on
-        democratizing travel knowledge, Tripscout provided users with up-to-date
-        travel guides and personalized recommendations, all for free. The app
-        received widespread acclaim, hailed as a "Game-Changing Travel App" by
-        purewow.com, and became a staple for travelers seeking inspiration and
-        guidance.
+        My role here was to design a captivating travel website that is modern,
+        easy to use and captures the attention of the use while showcasing the
+        brand's goals. To do so, I relied on a simple black color palette with
+        clean typography, clean layout and few but well placed animations to
+        showcase some personality.
       </p>
       <p className="text-xl leading-normal text-primary_customGray-900  ">
-        Building upon the success of the iOS app, we extended the seamless
-        travel experience to the web platform. The Tripscout web platform
-        retained the app's intuitive navigation and visually appealing design,
-        offering users a comprehensive travel planning tool accessible across
-        multiple devices. Key innovations included personalized bucket lists and
-        custom articles based on users interests, providing valuable insights
-        and inspiration for their journeys.
+        Seeing as how the design was great in Figma, I decided to continue the
+        process and tuned in to Visual Studio for the next step. To bring the
+        project to the web, I used React, Tailwind CSS and a little bit of
+        Framer Motion. Tailwind allows for true web design and makes the process
+        seamless. It shines so much that it offers advantages for futher design
+        exploration that design tools like Figma can't do at the moment.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        The front end was built with React, the most popular Javascript framer
+        made by Facebook. Routing was done with the React Router version 6.4.
+        This project was additionally enhanced with some simple animations with
+        Framer Motion, an animation library for React. Framer Motion makes
+        adding animations simple and it definitely adds to the overall
+        experience as long as they are carefully made and used. Feel free to
+        check out the final landing page on the web on the link below
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        The landing page was a great project to enhance and develop web
+        development as well web design skills in general. It showed me the power
+        of Tailwind and JSX, which made building a prototype in React super easy
+        yet so nice. Additionally, the usage of Framer Motion showed that
+        animations don't have to be complicated nor overdone to be effective and
+        add to the landing page.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        <a
+          href="https://travel-life-web.netlify.app/"
+          className="text-black hover:underline"
+        >
+          Travel Life Landing Page
+        </a>
       </p>
     </div>
   );
