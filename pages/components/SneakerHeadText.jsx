41951e501e2d26adb0a2f0078807fc65e46367e3
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-14 12:00:27 +0200

    mistake fixed

--- pages/components/SneakerHeadText.jsx
+++ pages/components/SneakerHeadText.jsx
@@ -2,8 +2,32 @@ import React from "react";
 
 const SneakerHeadText = () => {
   return (
-    <div className="max-w-screen-md mx-auto w-full h-full relative px-6 py-4">
-      SneakerHeadText
+    <div className="max-w-screen-md mx-auto w-full h-full flex flex-col justify-center items-start gap-6 relative px-6 py-4">
+      <p className="text-xl leading-normal text-primary_customGray-900 ">
+        @hotel, formerly known as TripScout, is a pioneering travel booking
+        platform that prioritizes user experience above all else. With a massive
+        social media presence and an active user base, @hotel offers unmatched
+        value with better prices, curated community insights, and superior
+        customer service. Recognized as the Travel Industry's Innovation of the
+        Year, @hotel has revolutionized the way travelers explore destinations
+        and plan their trips.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        After the rebranding to @hotel, the platform underwent a transformative
+        shift into a pioneering booking platform, offering exclusive rates and
+        deals to its members. As a product designer, my role was instrumental in
+        enhancing user experiences through comprehensive research, user testing,
+        and crafting intuitive user flows. I collaborated closely with
+        developers to translate design concepts into functional products.
+      </p>
+      <p className="text-xl leading-normal text-primary_customGray-900  ">
+        In addition to my design responsibilities, I also took on cross-team
+        initiatives, drove conversations, crafted prototypes to aid
+        decision-making, and successfully secured buy-in from stakeholders. I
+        led entire design initiatives from conception to production, involving
+        developers early in the process to gather their feedback, technical
+        constraints, and resource estimates.
+      </p>
     </div>
   );
 };
