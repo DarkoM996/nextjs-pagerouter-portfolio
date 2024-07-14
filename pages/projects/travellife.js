63610fbf9564d5923677a080a8dd765942dc494b
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-14 11:45:25 +0200

    latest

--- pages/projects/travellife.js
+++ pages/projects/travellife.js
@@ -1,4 +1,6 @@
 import React from "react";
+import BackToAllProjects from "../components/BackToAllProjects";
+import Footer from "../components/Footer";
 import TravelLifeHero from "../components/TravelLifeHero";
 import TravelLifeImages from "../components/TravelLifeImages";
 import TravelLifeText from "../components/TravelLifeText";
