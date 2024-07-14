3a3b104054e5f524ae1388535479e391ce6c5c38
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-14 16:32:03 +0200

    latest

--- pages/components/FurnicoImages.jsx
+++ pages/components/FurnicoImages.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import picture1 from "../../public/images/furniturewebsite-featured-pic-1.2.jpg";
 import picture2 from "../../public/images/furniturewebsite-featured-pic-1.3.jpg";
-import picture3 from "../../public/images/furniturewebsite-featured-pic-1.4.jpg";
-import picture4 from "../../public/images/furniturewebsite-featured-pic-1.5.jpg";
+import picture3 from "../../public/images/furniturewebsite-featured-pic-1.2.jpg";
+import picture4 from "../../public/images/furniturewebsite-featured-pic-1.6.jpg";
 import picture5 from "../../public/images/furniturewebsite-featured-pic-1.6.jpg";
-import picture6 from "../../public/images/furniturewebsite-featured-pic-1.7.jpg";
+import picture6 from "../../public/images/furniturewebsite-featured-pic-2.0.jpg";
 import picture7 from "../../public/images/furniturewebsite-featured-pic-2.5.jpg";
 import Image from "next/image";
 
