e8ae0547a1f2d6d6e825c92257c391081cea9b61
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-15 16:55:46 +0200

    latest

--- pages/components/SneakerHeadImages.jsx
+++ pages/components/SneakerHeadImages.jsx
@@ -11,84 +11,73 @@ import Image from "next/image";
 const SneakerHeadImages = () => {
   return (
     <div className="max-w-screen-xl mx-auto w-full h-full flex flex-col justify-center items-center gap-8 relative px-6 py-4">
-      {/* Image #1 */}
-      <div className="relative w-full h-screen py-12 ">
-        <Image
-          priority
-          fill
-          quality={100}
-          src={picture1}
-          alt="picture1"
-          className="object-cover w-full h-full rounded-3xl"
-        />
-      </div>
-      {/* Image #2 */}
-      <div className="relative w-full h-screen py-4 ">
-        <Image
-          priority
-          fill
-          quality={100}
-          src={picture2}
-          alt="picture2"
-          className="object-cover w-full h-full rounded-3xl"
-        />
-      </div>
-      {/* Image #3 */}
-      <div className="relative w-full h-screen py-4 ">
-        <Image
-          priority
-          fill
-          quality={100}
-          src={picture3}
-          alt="picture3"
-          className="object-cover w-full h-full rounded-3xl"
-        />
-      </div>
-      {/* Image #4 */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="relative w-full h-screen py-4 ">
+      {/* Grid */}
+      <div className="grid grid-cols-1 gap-6">
+        {/* Image #1 */}
+        <div className="relative w-full h-screen py-12 ">
           <Image
-            priority
-            fill
-            quality={100}
-            src={picture4}
-            alt="picture4"
+            priority={true}
+            src={picture1}
+            alt="/"
             className="object-cover w-full h-full rounded-3xl"
           />
         </div>
-        {/* Image #5 */}
+        {/* Image #2 */}
         <div className="relative w-full h-screen py-4 ">
           <Image
-            priority
-            fill
-            quality={100}
-            src={picture5}
-            alt="picture5"
+            priority={true}
+            src={picture2}
+            alt="/"
             className="object-cover w-full h-full rounded-3xl"
           />
         </div>
-      </div>
-      {/* Image #6 */}
-      <div className="relative w-full h-screen py-4 ">
-        <Image
-          priority
-          fill
-          quality={100}
-          src={picture6}
-          alt="picture6"
-          className="object-cover w-full h-full rounded-3xl"
-        />
-      </div>
-      {/* Image #7 */}
-      <div className="relative w-full h-screen py-4 ">
-        <Image
-          priority
-          fill={true}
-          quality={100}
-          src={picture7}
-          alt="picture7"
-          className="object-cover w-full h-full rounded-3xl"
-        />
+        {/* Image #3 */}
+        <div className="relative w-full h-screen py-4 ">
+          <Image
+            priority={true}
+            src={picture3}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
+        {/* Image #4 */}
+        <div className="grid grid-cols-2 gap-6">
+          <div className="relative w-full h-screen py-4 ">
+            <Image
+              priority={true}
+              src={picture4}
+              alt="/"
+              className="object-cover w-full h-full rounded-3xl"
+            />
+          </div>
+          {/* Image #5 */}
+          <div className="relative w-full h-screen py-4 ">
+            <Image
+              priority={true}
+              src={picture5}
+              alt="/"
+              className="object-cover w-full h-full rounded-3xl"
+            />
+          </div>
+        </div>
+        {/* Image #6 */}
+        <div className="relative w-full h-screen py-4 ">
+          <Image
+            priority={true}
+            src={picture6}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
+        {/* Image #7 */}
+        <div className="relative w-full h-screen py-4 ">
+          <Image
+            priority={true}
+            src={picture7}
+            alt="/"
+            className="object-cover w-full h-full rounded-3xl"
+          />
+        </div>
       </div>
     </div>
   );
