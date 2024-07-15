2734fd05ecf173b2e8d6f382d2567906ac23bc49
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-15 14:33:41 +0200

    font adjusted

--- pages/components/AboutHero.jsx
+++ pages/components/AboutHero.jsx
@@ -5,7 +5,7 @@ const AboutHero = () => {
     // In this case h-full is used so the space between the sections is what I needed per design requirements
     <div className="max-w-screen-md mx-auto w-full h-full relative px-6 py-6">
       <div className="flex flex-col justify-center items-start gap-6 lg:gap-16 lg:pt-16">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-medium">
           Creative Designer and Developer working on user-center innovation and
           product evolution
         </h1>
