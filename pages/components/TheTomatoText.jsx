010505b598eb9cf4252e43a30384ca55ad356bee
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-15 12:22:49 +0200

    latest

--- pages/components/TheTomatoText.jsx
+++ pages/components/TheTomatoText.jsx
@@ -4,19 +4,19 @@ const TheTomatoText = () => {
   return (
     <div className="max-w-screen-md mx-auto w-full h-full flex flex-col justify-center items-start gap-6 relative px-6 py-4 ">
       <p className="text-xl leading-normal text-primary_customGray-900 ">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet obcaecati
-        cum laboriosam quos laudantium, veniam impedit cupiditate sequi numquam
-        atque ab natus similique vitae iure illo, ut aliquam, hic autem.
+        The Tomato is a concept web app project about a food brand. The
+        challenge here was to create a clean user interface that is nice to look
+        at and relatively easy to use for just about anyone. Of course, this UI
+        was designed to be responsive and fit in across many different screen
+        sizes.
       </p>
       <p className="text-xl leading-normal text-primary_customGray-900  ">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet obcaecati
-        cum laboriosam quos laudantium, veniam impedit cupiditate sequi numquam
-        atque ab natus similique vitae iure illo, ut aliquam, hic autem.
-      </p>
-      <p className="text-xl leading-normal text-primary_customGray-900  ">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet obcaecati
-        cum laboriosam quos laudantium, veniam impedit cupiditate sequi numquam
-        atque ab natus similique vitae iure illo, ut aliquam, hic autem.
+        I approached this project by utilizing the brand's primary color and
+        just adding a tint of it in the grays as well as the whites for
+        consistency. The font of choice for the typography was Work Sans, which
+        had the needed several font weights which were needed for this project.
+        The focus of this project was to practice and improve general UI skills
+        for dashboards.
       </p>
     </div>
   );
