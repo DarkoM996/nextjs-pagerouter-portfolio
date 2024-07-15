4aeefd1913d0650f8c4e536afb55cf068507449b
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-15 19:03:24 +0200

    latest changes

--- pages/components/FurnicoImages.jsx
+++ pages/components/FurnicoImages.jsx
@@ -10,60 +10,60 @@ import picture7 from "../../public/images/furniturewebsite-featured-pic-2.5.jpg"
 
 const FurnicoImages = () => {
   return (
-    <div className="max-w-screen-xl mx-auto w-full min-h-screen relative px-6 py-4">
-      <div className="relative w-full h-4/5 py-12 ">
+    <div className="max-w-screen-xl mx-auto w-full h-full flex flex-col justify-between items-center gap-4 relative px-6 py-4">
+      <div className="relative w-full  h-[520px] md:h-[720px] py-12 ">
         <Image
           priority
           src={picture1}
           alt="picture1"
-          className="object-cover w-full  h-[600px] rounded-3xl"
+          className="object-cover w-full h-full rounded-3xl"
         />
       </div>
       {/* Image #2 */}
-      <div className="relative w-full h-4/5 py-4 ">
+      <div className="relative w-full h-full py-4 ">
         <Image
           priority
           src={picture2}
           alt="picture2"
-          className="object-cover w-full  h-[600px] rounded-3xl"
+          className="object-cover w-full   h-[520px] md:h-[720px] rounded-3xl"
         />
       </div>
       {/* Image #3 */}
-      <div className="relative w-full h-4/5 py-4 ">
+      <div className="relative w-full h-full py-4 ">
         <Image
           priority
           src={picture3}
           alt="picture3"
-          className="object-cover w-full  h-[600px] rounded-3xl"
+          className="object-cover w-full   h-[520px] md:h-[720px] rounded-3xl"
         />
       </div>
       {/* Image #4 */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="relative w-full h-4/5 py-4 ">
+        <div className="relative w-full h-full py-4 ">
           <Image
             priority
             src={picture4}
             alt="picture4"
-            className="object-cover w-full  h-[600px] rounded-3xl"
+            className="object-cover w-full   h-[520px] md:h-[720px] rounded-3xl"
           />
         </div>
         {/* Image #5 */}
-        <div className="relative w-full h-4/5 py-4 ">
+        <div className="relative w-full h-full py-4 ">
           <Image
             priority
             src={picture5}
             alt="picture5"
-            className="object-cover w-full  h-[600px] rounded-3xl"
+            className="object-cover w-full   h-[520px] md:h-[720px] rounded-3xl"
           />
         </div>
       </div>
       {/* Image #6 */}
-      <div className="relative w-full h-4/5 py-4 ">
+      <div className="relative w-full h-full py-4 ">
         <Image
           priority
           src={picture6}
           alt="picture6"
-          className="object-cover w-full  h-[600px] rounded-3xl"
+          className="object-cover w-full   h-[520px] md:h-[720px] rounded-3xl"
         />
       </div>
       {/* Image #7 */}
@@ -72,7 +72,7 @@ const FurnicoImages = () => {
           priority
           src={picture7}
           alt="picture7"
-          className="object-cover w-full h-[600px] rounded-3xl"
+          className="object-cover w-full  h-[520px] md:h-[720px] rounded-3xl"
         />
       </div>
     </div>
