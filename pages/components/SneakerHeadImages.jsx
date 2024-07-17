f2ff1cb6d6190c45b0ed591081ff1bd3c99c5751
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-17 17:10:36 +0200

    latest

--- pages/components/SneakerHeadImages.jsx
+++ pages/components/SneakerHeadImages.jsx
@@ -14,82 +14,82 @@ const SneakerHeadImages = () => {
       {/* Grid */}
       <div className="grid grid-cols-1 w- gap-6">
         {/* Image #1 */}
-        <div className="relative w-full h-full py-12 ">
+        <div className="relative w-full h-[520px] md:h-[720px] py-12 ">
           <Image
             priority
             src={picture1}
             width={1223}
             height={624}
             alt={picture1}
-            className="object-cover w-full h-[520px] md:h-[720px] rounded-3xl bg-center"
+            className="object-cover w-full h-full rounded-3xl bg-center"
           />
         </div>
         {/* Image #2 */}
-        <div className="relative w-full h-full py-4 ">
+        <div className="relative w-full h-[520px] md:h-[720px] py-4 ">
           <Image
             priority
             width={1223}
             height={624}
             src={picture2}
             alt={picture2}
-            className="object-cover w-full h-[520px] md:h-[720px] rounded-3xl bg-center"
+            className="object-cover w-full h-full rounded-3xl bg-center"
           />
         </div>
         {/* Image #3 */}
-        <div className="relative w-full h-full py-4 ">
+        <div className="relative w-full h-[520px] md:h-[720px] py-4 ">
           <Image
             priority
             width={1223}
             height={624}
             src={picture3}
             alt={picture3}
-            className="object-cover w-full h-[520px] md:h-[720px] rounded-3xl bg-center"
+            className="object-cover w-full h-full rounded-3xl bg-center"
           />
         </div>
         {/* Image #4 */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="relative w-full h-full py-4 ">
+          <div className="relative w-full h-[520px] md:h-[720px] py-4 ">
             <Image
               priority
               width={604}
               height={702}
               src={picture4}
               alt={picture4}
-              className="object-cover w-full h-[520px] md:h-[720px] rounded-3xl"
+              className="object-cover w-full h-full rounded-3xl"
             />
           </div>
           {/* Image #5 */}
-          <div className="relative w-full h-full py-4 ">
+          <div className="relative w-full h-[520px] md:h-[720px] py-4 ">
             <Image
               priority
               width={604}
               height={702}
               src={picture5}
               alt={picture5}
-              className="object-cover w-full h-[520px] md:h-[720px] rounded-3xl"
+              className="object-cover w-full h-full rounded-3xl"
             />
           </div>
         </div>
         {/* Image #6 */}
-        <div className="relative w-full h-full py-4 ">
+        <div className="relative w-full h-[520px] md:h-[720px] py-4 ">
           <Image
             priority
             width={1223}
             height={624}
             src={picture6}
             alt={picture6}
-            className="object-cover w-full h-[520px] md:h-[720px] rounded-3xl"
+            className="object-cover w-full h-full rounded-3xl"
           />
         </div>
         {/* Image #7 */}
-        <div className="relative w-full h-full py-4 ">
+        <div className="relative w-full h-[520px] md:h-[720px] py-4 ">
           <Image
             priority
             width={1223}
             height={624}
             src={picture7}
             alt={picture7}
-            className="object-cover w-full h-[520px] md:h-[720px] rounded-3xl"
+            className="object-cover w-full h-full rounded-3xl"
           />
         </div>
       </div>
