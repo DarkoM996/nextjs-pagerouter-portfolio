7cec4bc363a5d78a610853c03a4eb6439f2852fd
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-23 23:45:45 +0200

    latest

--- pages/components/ProjectSection.jsx
+++ pages/components/ProjectSection.jsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import React from "react";
-import picture1 from "../../public/images/furniturewebsite-featured-pic-2.5.jpg";
-import picture2 from "../../public/images/travel-landing-page-mockup-1.4.jpg";
-import picture3 from "../../public/images/sneakerhead-eccomerce-website-featured-picture-2.jpg";
+import picture1 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
+import picture2 from "../../public/images/travel-landing-page-featured-pic-(optimized)-2.1.jpg";
+import picture3 from "../../public/images/sneaker-head-featured-pic-(optimized)-2.3.jpg";
 import picture4 from "../../public/images/coffee-bar-concept-version-2.0-volume-2.3.jpg";
 import picture5 from "../../public/images/food-order-web-app-featured-mockup.jpg";
 import Link from "next/link";
@@ -15,12 +15,10 @@ const ProjectSection = () => {
         <div className="flex flex-col justify-center items-center gap-4 md:mb-40">
           <Link
             href="/projects/furnico"
-            className="relative w-full h-[320px] md:h-[720px]"
+            className="relative w-full h-[520px] md:h-[720px]"
           >
             <Image
               priority={true}
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               src={picture1}
               alt="picture1"
               className="w-full h-full object-cover rounded-3xl hover:scale-90  duration-500 transition-transform"
@@ -39,12 +37,10 @@ const ProjectSection = () => {
         <div className="flex flex-col justify-center items-center gap-4 md:mb-22">
           <Link
             href="/projects/travellife"
-            className="relative w-full h-[320px] md:h-[720px]"
+            className="relative w-full h-[520px] md:h-[720px]"
           >
             <Image
               priority={true}
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               src={picture2}
               alt="picture2"
               className="w-full h-full object-cover rounded-3xl hover:scale-90  duration-500 transition-transform"
@@ -63,12 +59,10 @@ const ProjectSection = () => {
         <div className="flex flex-col justify-center items-center gap-4 md:mb-36">
           <Link
             href="/projects/sneakerhead"
-            className="relative w-full h-[320px] md:h-[720px] "
+            className="relative w-full h-[520px] md:h-[720px] "
           >
             <Image
               priority={true}
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               src={picture3}
               alt="picture3"
               className="w-full h-full object-cover rounded-3xl hover:scale-90  duration-500 transition-transform"
@@ -87,12 +81,10 @@ const ProjectSection = () => {
         <div className="flex flex-col justify-center items-center gap-4 md:mb-22">
           <Link
             href="/projects/coffeebar"
-            className="relative w-full  h-[320px] md:h-[720px]"
+            className="relative w-full  h-[520px] md:h-[720px]"
           >
             <Image
               priority={true}
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               src={picture4}
               alt="picture4"
               className="w-full h-full object-cover rounded-3xl hover:scale-90  duration-500 transition-transform"
@@ -115,11 +107,9 @@ const ProjectSection = () => {
           <Link href="/projects/thetomato" className="relative w-full h-full">
             <Image
               priority={true}
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               src={picture5}
               alt="picture5"
-              className="w-full  h-[320px] md:h-[720px] object-cover rounded-3xl hover:scale-90  duration-500 transition-transform"
+              className="w-full  h-[520px] md:h-[720px] object-cover rounded-3xl hover:scale-90  duration-500 transition-transform"
             />
           </Link>
           <div className="flex flex-col justify-center items-center gap-2">
