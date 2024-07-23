48652f447d1eb0fbf4f91111736d3979badf2abe
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-23 23:18:23 +0200

    new images added.. testing

--- pages/components/FurnicoImages.jsx
+++ pages/components/FurnicoImages.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import picture1 from "../../public/images/furniturewebsite-featured-pic-2.5.jpg";
-import picture2 from "../../public/images/furniturewebsite-featured-pic-1.2.jpg";
-import picture3 from "../../public/images/furniturewebsite-featured-pic-1.4.jpg";
-import picture4 from "../../public/images/furniturewebsite-featured-pic-1.6.jpg";
-import picture5 from "../../public/images/furniturewebsite-featured-pic-1.9.jpg";
-import picture6 from "../../public/images/furniturewebsite-featured-pic-2.0.jpg";
-import picture7 from "../../public/images/furniturewebsite-featured-pic-2.5.jpg";
+import picture1 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
+import picture2 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.3.jpg";
+import picture3 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.2.jpg";
+import picture4 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.6.jpg";
+import picture5 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
+import picture6 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.0.jpg";
+import picture7 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.1.jpg";
 
 const FurnicoImages = () => {
   return (
